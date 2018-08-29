900ec876cb13ebef73596dc18617fda7d62b853a
Corey Lutrick <coreylutrick@gmail.com>
2018-08-28 18:39:08 -0700

    added classes and cleaned up code

---
+++ NameParser/Names/LastName.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NameParser.Names
+{
+    class LastName
+    {
+        public string Name { get; private set; }
+
+        public void GetName()
+        {
+            Console.WriteLine("Enter your last name.");
+            Name = Console.ReadLine();
+        }
+
+        public void PrintName()
+        {
+            var lastNameSpaces = "";
+
+            foreach (var letter in Name.ToUpper())
+            {
+                lastNameSpaces += letter.ToString().ToUpper() + " ";
+            }
+
+            Console.WriteLine(lastNameSpaces.Trim());
+        }
+    }
+}
